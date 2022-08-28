3b9418863148eb556ec56592811b7ef68c851ed4
bassamalraheam <88142583+bassamalraheam@users.noreply.github.com>
2022-08-28 07:15:51 +0300

    start working

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
 
 dotenv.config();
 
@@ -16,6 +18,9 @@ app.use(
 );
 app.use(bodyParser.json());
 
+app.use(morgan('tiny'));
+app.use(express.static('public'));
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
@@ -26,6 +31,16 @@ const connection = mongoose.connection;
 connection.once('open', async () => {
   console.log('MongoDB database connection established successfully');
 });
+
+app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: '/swagger.json',
+    },
+  })
+);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
