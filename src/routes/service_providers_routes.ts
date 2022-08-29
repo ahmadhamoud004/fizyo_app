08ce731a62799be2c7ea620cca86bfa8ff10cab8
bassamalraheam <88142583+bassamalraheam@users.noreply.github.com>
2022-08-29 17:49:17 +0300

    routes done

---
+++ src/routes/service_providers_routes.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import ServiceProvidersController from '../controllers/service_providers_controller';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const controller = new ServiceProvidersController();
+  controller
+    .getServiceProviders()
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((err) => {
+      res.status(422).send(err);
+    });
+});
+
+router.get('/:serviceProviderId', (req, res) => {
+  const controller = new ServiceProvidersController();
+  controller
+    .getServiceProvider(req.params.serviceProviderId)
+    .then((response) => res.send(response))
+    .catch((err) => res.status(404).send(err));
+});
+
+router.delete('/:serviceProviderId', (req, res) => {
+  const controller = new ServiceProvidersController();
+  controller
+    .deleteServiceProvider(req.params.serviceProviderId)
+    .then((response) => res.send(response))
+    .catch((err) => res.status(404).send(err));
+});
+
+router.post('/create', (req, res) => {
+  const controller = new ServiceProvidersController();
+  controller
+    .createServiceProvider(req.body)
+    .then((response) => res.send(response))
+    .catch((err) => res.status(422).send(err));
+});
+
+router.put('/update/:serviceProviderId', (req, res) => {
+  const controller = new ServiceProvidersController();
+  controller
+    .updateServiceProvider(req.params.serviceProviderId, req.body)
+    .then((response) => res.send(response))
+    .catch((err) => res.status(422).send(err));
+});
+
+export default router;
